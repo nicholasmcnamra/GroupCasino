73c45c26d339534b6f6ddd211141a4373f9a4672
Matt Kramer <matthew123003@gmail.com>
2024-03-19 07:35:00 -0400

    Adjusted methods

--- src/main/java/com/github/zipcodewilmington/utils/Craps.java
+++ src/main/java/com/github/zipcodewilmington/utils/Craps.java
@@ -13,7 +13,12 @@ public class Craps extends Dice {
     public int anySeven(int wager){
         d1 = rollDie(random);
         d2 = rollDie(random);
-        return 0;
+        if(d1 == 2 && d2 == 5 || d1 == 5 && d2 == 2 ||
+           d1 == 4 && d2 == 3 || d1 == 3 && d2 == 4 ||
+           d1 == 1 && d2 == 6 || d1 == 6 && d2 == 1){
+            return wager * 7;
+        }
+        return - wager;
     }
 
     public int bigSixAndEight(int wager){
@@ -25,16 +30,31 @@ public class Craps extends Dice {
     public int hardWays(int wager){
         d1 = rollDie(random);
         d2 = rollDie(random);
-        return 0;
+        if(d1 == 3 && d2 == 3 || d1 == 4 && d2 == 4){
+            return wager * 9;
+        }else if(d1 == 5 && d2 == 5 || d1 == 2 && d2 == 2){
+            return wager * 7;
+        }
+        return -wager;
+
     }
 
     public int oneRollBets(int wager){
         d1 = rollDie(random);
         d2 = rollDie(random);
-        return 0;
+        if(d1 == 1 && d2 == 1 || d1 == 6 && d2 == 6){
+            return wager * 30;
+        }else if(d1 == 1 && d2 == 2){
+            return wager * 15;
+        }else if(d1 == 2 && d2 == 1){
+            return wager * 15;
+        } else if(d1 == 5 && d2 == 6 || d1 == 6 && d2 == 5) {
+            return wager * 15;
+        }
+        return - wager;
     }
 
-    public int crapsBet(int wager){
+    public int anyCrapsBet(int wager){
         d1 = rollDie(random);
         d2 = rollDie(random);
         return 0;
