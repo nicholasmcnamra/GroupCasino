359ec26696a2983a370198820217e20bafdb9e00
moffd234 <moffd234@gmail.com>
2024-03-19 19:44:46 -0400

    Modified deck class so that it is populated with 52 cards when constructed

--- src/main/java/com/github/zipcodewilmington/casino/games/CardClasses/Deck.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/CardClasses/Deck.java
@@ -5,4 +5,17 @@ import com.github.zipcodewilmington.casino.games.CardClasses.Cards.Card;
 import java.util.Stack;
 
 public class Deck extends Stack<Card> {
+
+    public Deck(){
+        // Loop through each possible card value
+        for(int i = 0; i < 13; i++){
+            Cards.CardValue val = Cards.CardValue.values()[i];
+            // Iterate through the suites
+            for(int j = 0; j < 4; j++){
+                // Creates card with value CardValue i and Suite j
+                Card card = new Card(val, Cards.Suit.values()[j]);
+                add(card);
+            }
+        }
+    }
 }
--- src/test/java/com/github/zipcodewilmington/GameTests/CardClassesTest/DeckTests.java
+++ src/test/java/com/github/zipcodewilmington/GameTests/CardClassesTest/DeckTests.java
@@ -5,6 +5,8 @@ import com.github.zipcodewilmington.casino.games.CardClasses.Deck;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Collections;
 import java.util.Stack;
 
 public class DeckTests {
@@ -21,13 +23,29 @@ public class DeckTests {
 
     @Test
     public void testPop(){
-        Cards.Card newCard = new Cards.Card(Cards.CardValue.QUEEN, Cards.Suit.HEARTS);
-        deck.add(newCard);
-        Cards.Card expected = newCard;
+        Cards.Card expected = deck.peek();
 
         Cards.Card actual = deck.pop();
 
         Assert.assertEquals(expected, actual);
+    }
 
+    @Test
+    public void testShuffle(){
+
+        Deck tempDeck = (Deck) deck.clone();
+
+        boolean isEqual = true;
+
+        Collections.shuffle(deck);
+        for(int i = 0; i < deck.size(); i++){
+            Cards.Card original  = tempDeck.pop();
+            Cards.Card shuffled = deck.pop();
+            if(original != shuffled){
+                isEqual = false;
+            }
+        }
+
+        Assert.assertFalse(isEqual);
     }
 }
