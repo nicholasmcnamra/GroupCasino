2179547fe4dd38900c55c177168c600fe0a75deb
moffd234 <moffd234@gmail.com>
2024-03-20 14:07:47 -0400

    Made CardGamePlayer implement PlayerInterface

--- src/main/java/com/github/zipcodewilmington/casino/games/CardClasses/CardGamePlayer.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/CardClasses/CardGamePlayer.java
@@ -1,4 +1,41 @@
 package com.github.zipcodewilmington.casino.games.CardClasses;
 
-public class CardGamePlayer {
+import com.github.zipcodewilmington.casino.PlayerInterface;
+
+import java.util.Stack;
+
+public abstract class CardGamePlayer implements PlayerInterface {
+
+    private Hand hand;
+    double wagerAmount;
+
+
+
+    public Hand getHand() {
+        return hand;
+    }
+
+    public void setHand(Hand hand) {
+        this.hand = hand;
+    }
+
+    public double getWagerAmount() {
+        return wagerAmount;
+    }
+
+    public void setWagerAmount(double wagerAmount) {
+        this.wagerAmount = wagerAmount;
+    }
+
+    public int getHandTotal(){
+        return 0;
+    }
+
+    public Stack<Cards.Card> drawCards(int numToDraw){
+
+        return new Stack<>();
+    }
+
+    public void takeTurn(){}
+
 }
