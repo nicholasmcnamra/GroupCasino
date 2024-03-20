819b43f33d9522d280c8524295c006dea0cc9d9f
moffd234 <moffd234@gmail.com>
2024-03-20 14:00:11 -0400

    Completed add & remove tests for CardGameTests

---
+++ src/test/java/com/github/zipcodewilmington/GameTests/CardClassesTest/CardGameTests.java
@@ -0,0 +1,37 @@
+package com.github.zipcodewilmington.GameTests.CardClassesTest;
+
+import com.github.zipcodewilmington.casino.games.BlackJack.BlackJackPlayer;
+import com.github.zipcodewilmington.casino.games.CardClasses.CardGame;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CardGameTests {
+    CardGame cardGame;
+
+    @Before
+    public void setup(){
+        cardGame = new CardGame();
+    }
+
+    @Test
+    public void addPlayer(){
+
+        BlackJackPlayer bjp = new BlackJackPlayer();
+
+        cardGame.add(bjp);
+
+        Assert.assertTrue(cardGame.humanPlayers.contains(bjp));
+    }
+
+    @Test
+    public void testRemovePlayer(){
+
+        BlackJackPlayer bjp = new BlackJackPlayer();
+
+        cardGame.add(bjp);
+        cardGame.remove(bjp);
+
+        Assert.assertFalse(cardGame.humanPlayers.contains(bjp));
+    }
+}
