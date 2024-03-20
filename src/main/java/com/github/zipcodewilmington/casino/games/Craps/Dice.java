8d3c3fa2fd8971d2cffda0110a4b7dfe018cb8f0
Matt Kramer <matthew123003@gmail.com>
2024-03-20 15:37:00 -0400

    Finished craps test and dice tests

--- src/main/java/com/github/zipcodewilmington/casino/games/Craps/Dice.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/Craps/Dice.java
@@ -9,7 +9,7 @@ public class Dice implements RandomNumber {
 
     @Override
     public int randomNumberGenerator() {
-        return (int) (6.0 * Math.random() +1 );//Matt
+        return (int) (6.0 * Math.random() +1 );
     }
 
 
