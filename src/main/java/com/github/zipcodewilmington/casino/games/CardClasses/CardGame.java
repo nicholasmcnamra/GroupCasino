c815a2c75fe47cb4c9f0c8690920f0cdb35d9cce
moffd234 <moffd234@gmail.com>
2024-03-20 12:53:09 -0400

    Filled out CardGame superclass

--- src/main/java/com/github/zipcodewilmington/casino/games/CardClasses/CardGame.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/CardClasses/CardGame.java
@@ -1,4 +1,31 @@
 package com.github.zipcodewilmington.casino.games.CardClasses;
 
-public class CardGame {
+import com.github.zipcodewilmington.casino.GameInterface;
+import com.github.zipcodewilmington.casino.PlayerInterface;
+
+import java.util.ArrayList;
+
+public class CardGame implements GameInterface {
+    ArrayList<PlayerInterface> humanPlayers;
+    Dealer dealer;
+
+    @Override
+    public void add(PlayerInterface player) {
+        humanPlayers.add(player);
+    }
+
+    @Override
+    public void remove(PlayerInterface player) {
+        humanPlayers.remove(player);
+    }
+
+    public String printRules(){
+
+        return null;
+    }
+
+    @Override
+    public void run() {
+
+    }
 }
