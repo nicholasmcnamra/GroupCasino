70cfb958a3c0660e0b5cda49656b099fd978ff0d
nicholasmcnamra <nikomcnamara@gmail.com>
2024-03-20 12:01:52 -0400

    starting CasinoAccountTests and CasinoAccount

--- src/main/java/com/github/zipcodewilmington/casino/Accounts/CasinoAccount.java
+++ src/main/java/com/github/zipcodewilmington/casino/Accounts/CasinoAccount.java
@@ -1,9 +1,89 @@
 package com.github.zipcodewilmington.casino.Accounts;
 
+import java.util.Date;
+
 /**
  * Created by leon on 7/21/2020.
  * `ArcadeAccount` is registered for each user of the `Arcade`.
  * The `ArcadeAccount` is used to log into the system to select a `Game` to play.
  */
 public class CasinoAccount {
+    private int personId;
+    public String getAccountName;
+    private String first_name;
+    private String last_name;
+    private Date birthDate;
+    private String accountName;
+    private String accountPassword;
+    private double accountBalance = 0.0;
+
+    public CasinoAccount(int personId, String first_name, String last_name, Date birthDate, String accountName, String accountPassword, double accountBalance) {
+        this.personId = personId;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.birthDate = birthDate;
+        this.accountName = accountName;
+        this.accountPassword = accountPassword;
+        this.accountBalance = accountBalance;
+    }
+
+    public int getPersonId() {
+        return personId;
+    }
+
+    public void setPersonId(int personId) {
+        this.personId = personId;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getAccountName() {
+        return accountName;
+    }
+
+    public void setAccountName(String accountName) {
+        this.accountName = accountName;
+    }
+
+    public String getAccountPassword() {
+        return accountPassword;
+    }
+
+    public void setAccountPassword(String accountPassword) {
+        this.accountPassword = accountPassword;
+    }
+
+    public double getAccountBalance() {
+        return accountBalance;
+    }
+
+    public void setAccountBalance(double accountBalance) {
+        this.accountBalance = accountBalance;
+    }
+
+    public double showBalance() {
+        return accountBalance;
+    }
 }
---
+++ src/test/java/com/github/zipcodewilmington/GameTests/CasinoAccountTests.java
@@ -0,0 +1,32 @@
+package com.github.zipcodewilmington.GameTests;
+import com.github.zipcodewilmington.casino.Accounts.CasinoAccount;
+import org.junit.Test;
+import org.junit.Assert;
+
+import java.util.Date;
+
+
+public class CasinoAccountTests {
+
+    @Test
+    public void showBalanceTest() {
+        Date date = new Date(9/30/1946);
+        CasinoAccount account1 = new CasinoAccount(1, "Hector", "Lavoe", date, "hl", "salsa", 100.0);
+
+        double expectedBalance = 100.0;
+        double actualBalance = account1.showBalance();
+
+        Assert.assertEquals(expectedBalance, actualBalance, 0.00001);
+    }
+
+    @Test
+    public void loginTest() {
+        Date date = new Date(9/30/1946);
+        CasinoAccount account1 = new CasinoAccount(1, "Hector", "Lavoe", date, "hl", "salsa", 100.0);
+
+        String expectedAccountName = "hl";
+        String actualAccountName = account1.getAccountName();
+        Assert.assertEquals(expectedAccountName, actualAccountName);
+    }
+
+}
