a272c4aba9cb881e501b55c13f9a2fffffc2d1fe
ali-b14 <alib714@gmail.com>
2024-03-20 14:11:47 -0400

    Added tests and methods for Wheel and Roulette

--- src/main/java/com/github/zipcodewilmington/casino/games/Roulette/RouletteGame.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/Roulette/RouletteGame.java
@@ -1,4 +1,31 @@
 package com.github.zipcodewilmington.casino.games.Roulette;
 
-public class RouletteGame {
+import com.github.zipcodewilmington.casino.GameInterface;
+import com.github.zipcodewilmington.casino.PlayerInterface;
+import com.github.zipcodewilmington.casino.games.RandomNumber;
+
+public class RouletteGame implements GameInterface {
+    private String printRules = "Welcome to Roulette! Adapted for the non-gambling community, see if you can guess what number the ball will stop on.";
+
+    public RouletteGame() {
+    }
+
+
+    public String getRules() {
+        return printRules;
+    }
+    @Override
+    public void run() {
+
+    }
+    @Override
+    public void add(PlayerInterface player) {
+
+    }
+
+    @Override
+    public void remove(PlayerInterface player) {
+
+    }
+
 }
--- src/test/java/com/github/zipcodewilmington/GameTests/RouletteTests/WheelTest.java
+++ src/test/java/com/github/zipcodewilmington/GameTests/RouletteTests/WheelTest.java
@@ -1,4 +1,16 @@
 package com.github.zipcodewilmington.GameTests.RouletteTests;
+import com.github.zipcodewilmington.casino.games.Roulette.Wheel;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class WheelTest {
+    @Test
+    public void testSpinWheel() {
+        Wheel wheel = new Wheel();
+        int[] expectedRandomNumber = new int[]{26, 3, 35, 12, 28, 7, 29, 18, 22, 9, 31, 14, 20, 1, 33, 16, 24, 5, 10, 23, 8, 30, 11, 36, 13, 27, 6, 34, 17, 25, 2, 21, 4, 19, 15, 32};
+
+        int actualRandomNumber = wheel.spinWheel();
+
+        Assert.assertEquals(expectedRandomNumber,actualRandomNumber);
+    }
 }
