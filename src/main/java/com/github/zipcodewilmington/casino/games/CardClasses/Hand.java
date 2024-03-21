64ab73f40a28ebe10481798c8471b24255ac6b08
moffd234 <moffd234@gmail.com>
2024-03-20 21:48:09 -0400

    Added inheritance to hand class

--- src/main/java/com/github/zipcodewilmington/casino/games/CardClasses/Hand.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/CardClasses/Hand.java
@@ -1,4 +1,12 @@
 package com.github.zipcodewilmington.casino.games.CardClasses;
 
-public class Hand {
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Hand extends ArrayList<Cards.Card> {
+
+    public Hand(){}
+    public Hand(Cards.Card... cards){
+        this.addAll(Arrays.asList(cards));
+    }
 }
