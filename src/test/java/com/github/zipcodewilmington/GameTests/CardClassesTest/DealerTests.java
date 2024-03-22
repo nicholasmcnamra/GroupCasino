247c37992e0f4ce63112306b17e620015881e642
moffd234 <moffd234@gmail.com>
2024-03-21 21:28:31 -0400

    Merged from upstream

---
+++ src/test/java/com/github/zipcodewilmington/GameTests/CardClassesTest/DealerTests.java
@@ -0,0 +1,13 @@
+package com.github.zipcodewilmington.GameTests.CardClassesTest;
+
+/*
++ shuffleDeck(Deck deck)
+- deal()  returns ArrayList<Hand>
++ boolean takeTurn() return boolean
++ hit() returns Card
++ ArrayList<Cards> getHand() returns hand
++ Card compareCards() returns winner
+ */
+
+public class DealerTests {
+}
